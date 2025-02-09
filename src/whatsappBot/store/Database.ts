6043f09cedae0b61f090292124757f64d5a1fe84
Rbto-flores <roberto.floresrosas@pm.me>
2025-02-09 12:18:50 -0400

    remove store folder

--- src/whatsappBot/store/Database.ts
+++
@@ -1,7 +0,0 @@
-import repository from "../../database/repository/index.js"
-import type { CreateContactType } from "../converter/receivedContacts.js"
-
-
-export async function saveManyContacts(contacts: CreateContactType[]): Promise<void> {
-  await repository.contacts.insertManyContacts(contacts)
-}
